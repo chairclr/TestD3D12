30b701d2027c8bee02e02b5cbb2afc78c0b934b4
chairclr <chairclr@gmail.com>
2025-04-01 16:51:05 -0400

    Switch to use Publicizer for DescriptorHeapHelper

--- MiniEngine/Graphics/DescriptorHeapHelper.cs
+++ MiniEngine/Graphics/DescriptorHeapHelper.cs
@@ -1,39 +1,44 @@
-using System.Reflection;
+using SharpGen.Runtime;
 using Vortice.Direct3D12;
 
 namespace MiniEngine.Graphics;
 
 public static class DescriptorHeapHelper
 {
-    private static readonly MethodInfo getVtbl = (typeof(ID3D12DescriptorHeap)
-                .GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .First(static x => x.Name == "Item")).GetMethod!;
+    private const uint GetCPUDescriptorHandleForHeapStart__vtbl_index = 9;
+    private const uint GetGPUDescriptorHandleForHeapStart__vtbl_index = 10;
 
     public static CpuDescriptorHandle GetCPUDescriptorHandleForHeapStart1(this ID3D12DescriptorHeap heap)
     {
         CpuDescriptorHandle result;
 
-        // Fixed behavior of normal Vortice ID3D12DescriptorHeap::GetCPUDescriptorHandleForHeapStart
-        unsafe
+        if (PlatformDetection.IsItaniumSystemV)
         {
-            void* nativeCall = Pointer.Unbox(getVtbl.Invoke(heap, [9])!);
-
-            ((delegate* unmanaged[Stdcall]<nint, void*, void*>)nativeCall)(heap.NativePointer, &result);
-            return result;
+            // Fixed behavior of normal Vortice ID3D12DescriptorHeap::GetCPUDescriptorHandleForHeapStart
+            unsafe
+            {
+                var fn = (delegate* unmanaged[Stdcall]<nint, CpuDescriptorHandle*, CpuDescriptorHandle*>)heap[GetCPUDescriptorHandleForHeapStart__vtbl_index];
+                return *fn(heap.NativePointer, &result);
+            }
         }
+
+        return heap.GetCPUDescriptorHandleForHeapStart();
     }
 
     public static GpuDescriptorHandle GetGPUDescriptorHandleForHeapStart1(this ID3D12DescriptorHeap heap)
     {
         GpuDescriptorHandle result;
 
-        // Fixed behavior of normal Vortice ID3D12DescriptorHeap::GetGPUDescriptorHandleForHeapStart
-        unsafe
+        if (PlatformDetection.IsItaniumSystemV)
         {
-            void* nativeCall = Pointer.Unbox(getVtbl.Invoke(heap, [10])!);
-
-            ((delegate* unmanaged[Stdcall]<nint, void*, void*>)nativeCall)(heap.NativePointer, &result);
-            return result;
+            // Fixed behavior of normal Vortice ID3D12DescriptorHeap::GetGPUDescriptorHandleForHeapStart
+            unsafe
+            {
+                var fn = (delegate* unmanaged[Stdcall]<nint, GpuDescriptorHandle*, GpuDescriptorHandle*>)heap[GetGPUDescriptorHandleForHeapStart__vtbl_index];
+                return *fn(heap.NativePointer, &result);
+            }
         }
+
+        return heap.GetGPUDescriptorHandleForHeapStart();
     }
 }
